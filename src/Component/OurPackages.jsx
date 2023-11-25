94e358f72097a704335eafe2dbfd5f8da9a440eb
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-11-25 17:09:21 +0600

    set the heart icon on the image

--- src/Component/OurPackages.jsx
+++ src/Component/OurPackages.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ButtonTitle from "./ButtonTitle";
-
+import { FaHeart } from "react-icons/fa";
+import './OurPackage.css'
 
 const OurPackages = () => {
 
@@ -17,7 +18,12 @@ const OurPackages = () => {
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
             {
                 packages.map(data => <div key={data._id} className="card bg-base-100 shadow-xl">
-                <figure><img className="h-[230px] w-full" src={data.img} alt="Shoes" /></figure>
+                <div className="container hover:bg-black">
+                <figure><img className="h-[230px] image w-full" src={data.img} alt="Shoes" /></figure>
+                <div className="middle">
+                <button><FaHeart className="text-6xl text-[#FF0904]"></FaHeart></button>
+                </div>
+                </div>
                 <div className="card-body">
                   <h2 className="card-title">{data.tourType}</h2>
                   <h2 className="card-title">{data.tripTitle}</h2>
